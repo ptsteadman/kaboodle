226fc1ec72232da22718cc6343e8760c1e1f685a
Patrick Steadman <ptsteadman@gmail.com>
2015-08-03 23:29:12 -0400

    initial commit and rename

---
+++ kaboodle.js
@@ -0,0 +1,4 @@
+window._kreatives = window._kreatives || [];
+window._kreatives.push({'title': 'test', 'creative': 'creatives/trump.jpg'});
+document.write("<h1>" + _kreatives[0]['title'] + "</h1>");
+document.write("<img src='" + _kreatives[0]['creative'] + "'/>");
